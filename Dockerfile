fc804ca66e78d3cbd924108df485af32ae35f105
Thanikul Sriuthis <dekitclub@gmail.com>
2020-11-24 12:12:05 +0700

    php7.3-apache

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM php:7.3-apache
+
+RUN apt-get update && \
+    apt-get install -y \
+    libfreetype6-dev \
+    libgd-dev \
+    libjpeg62-turbo-dev \
+    libpng-dev \
+    libjpeg-dev
+
+RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+RUN docker-php-ext-install -j$(nproc) gd
---
+++ code/index.php
@@ -0,0 +1,38 @@
+﻿<?php
+error_reporting(E_ALL ^E_NOTICE);
+mb_internal_encoding('UTF-8');
+// if ($_COOKIE['survey']) {
+//     $GET_SURVEY = true;
+// } else {
+//     if (empty($_GET['f'])) {
+//         $GET_SURVEY = false;
+//     } else {
+//         $GET_SURVEY = true;
+//         setcookie("survey", true);
+//     }
+// }
+// setcookie("survey", 0, 0);
+?>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+
+<title>Checklist! Docker</title>
+<style type="text/css">
+html, body { height: 100%; width:100%; margin:0; padding:0; }
+</style>
+
+<link rel="shortcut icon" href="favicon.ico">
+</head>
+
+<body>
+<iframe width="100%" height="100%" frameborder="0" src="filemanager/dialog.php?type=2&editor=mce_0&lang=en_EN&fldr=">Loading...</iframe>
+</body>
+</html>
